7461c45b8d9976ea1d3092137f83fe522552ea98
nguyenthanhtoan903 <codehub.personal@gmail.com>
2024-03-28 12:33:14 +0700

    final

---
+++ contactbook-frontend/src/views/ContactAdd.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-if="contact" class="page">
+    <h4>Thêm mới Liên hệ</h4>
+    <ContactForm :contact="contact" @submit:contact="addContact" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+  components: {
+    ContactForm,
+  },
+  data() {
+    return {
+      contact: {},
+      message: "",
+    };
+  },
+  methods: {
+    async addContact(data) {
+      try {
+        await ContactService.create(data);
+        this.message = "Liên hệ được tạo thành công.";
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
